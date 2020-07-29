faf52823acb72038046d0fe640a7de82273ef551
Alex Jurkiewicz <alex@jurkiewi.cz>
2020-07-29 13:40:08 +1000

    Revert "Remove password output"
    This reverts commit 85218db29e056598b57ddfb81bdd5c23344b0ec0.

--- output.tf
+++ output.tf
@@ -2,3 +2,9 @@ output "ssm_parameter" {
   description = "Path of the SSM parameter that holds the user password."
   value       = var.deploy == true ? aws_ssm_parameter.param[0].name : null
 }
+
+output "password" {
+  description = "Generated password for the user."
+  value       = random_password.password.result
+  sensitive   = true
+}
