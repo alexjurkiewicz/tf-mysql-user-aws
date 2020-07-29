06274e0cd00b9fc57e5babaed3f22554fad813a1
Alex Jurkiewicz <alex@jurkiewi.cz>
2020-07-29 13:40:41 +1000

    Fix logic for ssm_parameter output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "ssm_parameter" {
-  description = "Path of the SSM parameter that holds the user password."
-  value       = var.deploy == true ? aws_ssm_parameter.param[0].name : null
+  description = "Path of the SSM parameter that holds the user password (if ssm_prefix was defined)."
+  value       = (var.deploy == true && var.ssm_prefix != null) ? aws_ssm_parameter.param[0].name : null
 }
 
 output "password" {
